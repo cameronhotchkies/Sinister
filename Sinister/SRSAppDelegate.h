54ef85decaccae7389a4bb615e338b6037c6c76f
Cameron Hotchkies <cameron@srs.bizn.as>
2014-01-18 11:24:44 -0600

    Initial Commit

---
+++ Sinister/SRSAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  SRSAppDelegate.h
+//  Sinister
+//
+//  Created by Cameron Hotchkies on 1/18/14.
+//  Copyright (c) 2014 Cameron Hotchkies. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SRSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Sinister/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Sinister
+//
+//  Created by Cameron Hotchkies on 1/18/14.
+//  Copyright (c) 2014 Cameron Hotchkies. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
