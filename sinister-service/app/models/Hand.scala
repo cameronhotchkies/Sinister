8d631475b9607295818211dcbd439202b4679d36
Cameron Hotchkies <cameron@semisafe.com>
2021-01-24 23:17:03 -0800

    feat: Save hand results into folders for each player (#18)
    This is a fairly naive implementation that is fairly large, but will handle
    faster lookup for players in isolation.
    
    This saves the play data in murmur3 hash directories based on the player name
    to remove any issues with oddly formatted player names.

--- sinister-service/app/models/Hand.scala
+++ sinister-service/app/models/Hand.scala
@@ -4,7 +4,7 @@ import io.circe.Encoder
 import io.circe.generic.semiauto.deriveEncoder
 import models.gamestate.GameStateEvent
 
-case class Hand(summary: HandSummary, events: Seq[GameStateEvent]) {}
+case class Hand(summary: HandSummary, events: Seq[GameStateEvent], sources: Seq[String]) {}
 
 object Hand {
   implicit val encoder: Encoder[Hand] = deriveEncoder
