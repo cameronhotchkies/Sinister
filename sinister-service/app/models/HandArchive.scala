d90777e33b46ccce4ba3ce27b4e028fe20d3d419
Cameron Hotchkies <cameron@semisafe.com>
2021-01-27 21:01:13 -0800

    refactor: rename Hand to HandArchive
    This is a series to make the hand / summary clearer.

---
+++ sinister-service/app/models/HandArchive.scala
@@ -0,0 +1,12 @@
+package models
+
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.{Decoder, Encoder}
+import models.gamestate.HandEvent
+
+case class HandArchive(summary: HandSummary, events: Seq[HandEvent], sources: Seq[String]) {}
+
+object HandArchive {
+  implicit val encoder: Encoder[HandArchive] = deriveEncoder
+  implicit val decoder: Decoder[HandArchive] = deriveDecoder
+}
