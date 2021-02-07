8183891af05f2728e75781b073a3a1e7848a9219
Cameron Hotchkies <cameron@semisafe.com>
2021-02-06 22:24:00 -0800

    feat: Added statistics for wins
    Mostly post-flop win stats like
    WTSD, W$SD

---
+++ sinister-service/app/models/HeroHand.scala
@@ -0,0 +1,106 @@
+package models
+
+import io.circe.syntax.EncoderOps
+import io.circe.{Encoder, Json}
+import models.Hand.logger
+import models.gamestate.playeraction.Fold
+import models.gamestate._
+
+case class HeroHand(hero: String, hand: Hand) {
+  private val position = hand.positionForPlayer(hero)
+
+  def bigBlindsWon(): Option[BigDecimal] = {
+
+    val chipMovementEvents = hand.events.filter {
+      case subtractChipsFromStack: SubtractChipsFromStack =>
+        subtractChipsFromStack.seatIndex == position
+      case subtractChipsFromPot: SubtractChipsFromPot =>
+        subtractChipsFromPot.seatIndex == position
+      case _ => false
+    }
+
+    val chipDelta = chipMovementEvents.foldLeft(BigDecimal(0)) { (acc, event) =>
+      event match {
+        case SubtractChipsFromStack(_, chips)  => acc - chips
+        case SubtractChipsFromPot(_, _, chips) => acc + chips
+      }
+    }
+
+    hand.table.map { tbl =>
+      chipDelta / tbl.bigBlind
+    }
+  }
+
+  def wentToShowdown(): Option[Boolean] = {
+    if (hand.stages.contains(1)) {
+
+      val collected = hand.events.collectFirst {
+        case EnterNextStage(_)              => true
+        case Fold(seat) if seat == position => false
+      }
+
+      collected.flatMap { flopEntered =>
+        if (flopEntered) {
+          val seatsInShowdown = hand.playersInvolvedInShowdown
+
+          Option(seatsInShowdown.contains(position))
+        } else {
+          None
+        }
+      }
+    } else {
+      logger.info("[.] No Flop")
+      None
+    }
+  }
+
+  def postFlopEvents: Seq[HandEvent] = {
+    hand.events.dropWhile(_.isInstanceOf[EnterNextStage] == false)
+  }
+
+  def wonWhenSawFlop(): Option[Boolean] = {
+    val pfe = postFlopEvents
+    if (pfe.nonEmpty) {
+
+      val winEvents = postFlopEvents
+        .filter(_.isInstanceOf[WinHand])
+        .filter(_.asInstanceOf[WinHand].seatIndex == position)
+      Option(winEvents.nonEmpty)
+    } else {
+      None
+    }
+  }
+
+  def w$sd(): Option[Boolean] = {
+    wentToShowdown().flatMap { relevant =>
+      {
+        if (relevant) {
+          Option(
+            hand.events
+              .collectFirst {
+                case WinHand(seat, _, _) if seat == position => true
+              }
+              .getOrElse(false)
+          )
+        } else {
+          None
+        }
+      }
+    }
+  }
+}
+
+object HeroHand {
+  implicit val encoder: Encoder[HeroHand] = (heroHand: HeroHand) => {
+    Hand
+      .encoder(heroHand.hand)
+      .deepMerge(
+        Json.obj(
+          "wtsd" -> heroHand.wentToShowdown().asJson,
+          "wwsf" -> heroHand.wonWhenSawFlop().asJson,
+          "w$sd" -> heroHand.w$sd().asJson,
+          "bbWon" -> heroHand.bigBlindsWon().asJson
+        )
+      )
+  }
+}
