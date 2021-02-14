11e75c810128aa34a11d67d3a5527cbf19b26f62
Cameron Hotchkies <cameron@semisafe.com>
2021-02-13 22:02:09 -0800

    feat: Add capability to manually add tables and inspect registries (#38)

--- sinister-service/app/controllers/DiagnosticController.scala
+++ sinister-service/app/controllers/DiagnosticController.scala
@@ -1,19 +1,21 @@
 package controllers
 
+import actors.PlayerRegistry.{PlayerSeen, RecentPlayers}
+import actors.TableRegistry.ListTables
 import actors.{ActorRoot, TableRegistry}
-import io.circe.{Json, parser}
+import akka.pattern.ask
+import akka.util.Timeout
+import io.circe.parser
+import io.circe.syntax.EncoderOps
 import models.Table
 import play.api.Logger
 import play.api.libs.circe.Circe
-import play.api.mvc.{
-  Action,
-  AnyContent,
-  BaseController,
-  ControllerComponents,
-  Request
-}
+import play.api.mvc._
 
 import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
 
 class DiagnosticController @Inject() (
     val controllerComponents: ControllerComponents,
@@ -36,5 +38,55 @@ class DiagnosticController @Inject() (
       }
 
       Ok("")
+    }
+
+  def listTables(): Action[AnyContent] =
+    Action.async {
+
+      implicit val timeout: Timeout = 6 seconds
+      implicit val ec: ExecutionContext = controllerComponents.executionContext
+
+      val response = actorRoot.tableRegistry ? ListTables
+
+      response.map {
+        case tables: Seq[Table] =>
+          Ok(tables.asJson)
+      }
+    }
+
+  def recentPlayers(): Action[AnyContent] =
+    Action.async {
+
+      implicit val timeout: Timeout = 6 seconds
+      implicit val ec: ExecutionContext = controllerComponents.executionContext
+
+      val response = actorRoot.playerRegistry ? RecentPlayers
+
+      response.map {
+        case players: Seq[String] =>
+          Ok(players.asJson)
+      }
+    }
+
+  def setRecentPlayers(): Action[AnyContent] =
+    Action.async { request =>
+      implicit val timeout: Timeout = 6 seconds
+      implicit val ec: ExecutionContext = controllerComponents.executionContext
+
+      val postBody = request.body.asText.getOrElse("")
+      val decoded = parser.decode[Seq[String]](postBody)
+
+      decoded.map { players: Seq[String] =>
+        players.foreach { player =>
+          actorRoot.playerRegistry ! PlayerSeen(player)
+        }
+      }
+
+      val response = actorRoot.playerRegistry ? RecentPlayers
+
+      response.map {
+        case players: Seq[String] =>
+          Ok(players.asJson)
+      }
     }
 }
